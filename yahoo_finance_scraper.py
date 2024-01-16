27b4c47082f93d6dde8f8ce7b739a189f6b4326e
martuy25 <133281613+martuy25@users.noreply.github.com>
2024-01-15 18:32:29 -0600

    Update yahoo_finance_scraper.py

--- yahoo_finance_scraper.py
+++ yahoo_finance_scraper.py
@@ -21,8 +21,8 @@ class YahooFinanceScrapper:
 				rows = table.find_all('tr')
 				for row in rows:
 					columns = row.find_all(['th', 'td'])
-					if len(columns) ==2:
-						self.financial_data[columns[0].text.strip()] = columns[1].text.strip()
+					if len(columns) ==2: # if true, adds the data to 'self.finacial_data' dictionary
+						self.financial_data[columns[0].text.strip()] = columns[1].text.strip() #this a check to ensure only rows w/ 2 columns are processed
 		else:
 			print(f"Failed to retrieve the page. Status code {response.status_code}")
 	def print_data(self): #function to print the extracted data
@@ -31,14 +31,14 @@ class YahooFinanceScrapper:
 		print("Financial Statement Data: ", self.financial_data)
 		
 	def save_to_csv(self):
-		with open('stock_data.csv', 'w' , newline= '') as csvfile:
-			fieldnames = ['Attribute', 'Value']
-			writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		with open('stock_data.csv', 'w' , newline= '') as csvfile: #opens a CSV file in write mode, "newline=''" is used to ensure newline characters handled properly in csv file
+			fieldnames = ['Attribute', 'Value'] #defiing the column headers
+			writer = csv.DictWriter(csvfile, fieldnames=fieldnames) #creates a CSV writer for 'stock_data.csv' and makes the file have columns 'Attribute' and 'Value"
 			
-			writer.writeheader()
-			for key, value in self.stock_info.items():
+			writer.writeheader() #writes the header row
+			for key, value in self.stock_info.items(): # writes the stock info into the CSV
 				writer.writerow({'Attribute': key, 'Value': value})
-			for key, value in self.financial_data.items():
+			for key, value in self.financial_data.items(): #writes the financial data to the CSV
 				writer.writerow({'Attribute': key, 'Value': value})
 		
 			

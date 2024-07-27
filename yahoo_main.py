8cfc98d626896c7631890225aaead0f6727b7ce7
martuy25 <133281613+martuy25@users.noreply.github.com>
2024-07-27 18:17:27 -0400

    Update yahoo_main.py

--- yahoo_main.py
+++ yahoo_main.py
@@ -1,14 +1,24 @@
 #!/bin/python3
 
 import sys
-from yahoo_finance_scraper import YahooFinanceScrapper
+from yahoo_finance_scraper import YahooFinanceScraper
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("Program takes two arguments, please try again")
-	else:
-		url = sys.argv[1] #gets the url from cmd Line
-		scraper = YahooFinanceScrapper(url)
-		scraper.scrape_stock_data()
-		scraper.print_data()
-		scraper.save_to_csv()
+    base_url = "https://finance.yahoo.com/quote/"
+
+    # Combining all arguments into a single string 
+    input_tickers = ' '.join(sys.argv[1:])
+    ticker_symbols = input_tickers.replace(',', ' ').split()
+
+    # convert ticker to uppercase
+    ticker_symbols = [symbol.upper() for symbol in ticker_symbols if symbol]
+
+    if not ticker_symbols:
+        ticker_symbols = ["BTC-USD", "MSFT", "SPY"]  # Default ticker symbols
+
+    stock_urls = [f"{base_url}{symbol}?p={symbol}&.tsrc=fin-srch" for symbol in ticker_symbols]
+
+    scraper = YahooFinanceScraper(stock_urls)
+    scraper.scrape_stock_data()
+    scraper.print_data()
+    scraper.save_to_csv()
